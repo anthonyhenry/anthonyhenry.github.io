3d97daa4585574c8944166110d05472edc2944f4
anthonyhenry <anthonyh1497@gmail.com>
2022-04-25 23:40:51 -0700

    Days Javascript

--- 100/days.js
+++ 100/days.js
@@ -1,12 +1,42 @@
-const startDate = new Date('4/18/2022');
+const startDate = new Date("4/18/2022");
 
-const endDate = new Date('7/26/2022');
+const endDate = new Date("7/26/2022");
 
 const todaysDate = new Date();
 
-const diffTime = Math.abs(todaysDate - endDate);
+var output;
 
-const daysLeft = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+//Check if the 100 days have ended
+if(todaysDate > endDate)
+{
+	output = formatDate(startDate) + " - " + formatDate(endDate);
+	document.querySelector("#days").innerHTML = output;
+}
+//If the 100 days has not passed, display the remaining days
+else
+{
+	const diffTime = Math.abs(todaysDate - endDate);
 
+	output = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-document.querySelector("#daysRemaining").innerHTML = daysLeft;
+	document.querySelector("#countdown").innerHTML = output;
+}
+
+function formatDate(dateToFormat){
+	var day = dateToFormat.getDate();
+	var month = dateToFormat.getMonth() + 1 //January is 0!
+	var year = dateToFormat.getFullYear();
+
+	if (day < 10)
+	{
+		day = "0" + day;
+	}
+	if(month < 10)
+	{
+		month = "0" + month;
+	}
+
+	const formattedDate = month + "/" + day + "/" + year;
+
+	return formattedDate;
+}
