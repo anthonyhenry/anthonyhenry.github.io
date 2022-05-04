3849b7523e3b5907fbd9a993256adf43cb55ad84
anthonyhenry <anthonyh1497@gmail.com>
2022-05-04 00:13:56 -0700

    Random number generation
    Created a function for generating random numbers. Implemented it to generate a random background color
    
    Commented out a bunch of code for reference later

--- 100/Projects/Skull/sketch.js
+++ 100/Projects/Skull/sketch.js
@@ -1,16 +1,124 @@
+// 9 colors min, 14 max
+// 360/9 = 40
+//
+// H: 0-320 ; Diff: 
+// S: 30-100 ; Diff: 60
+// B: 40-100 ; Diff: 10
+
+// function getRandomColor(){
+// 	//Choose random HSB values
+// 	var randomH = random(0, 320);
+// 	var randomS = random(30, 100);
+// 	var randomB = random(30, 100); change to 40 if needed
+
+// 	const hDifference = 20;
+// 	const sDifference = 30;
+// 	const bDifference = 15;
+
+// 	for(var i = 0; i < H.length; i++)
+// 	{
+// 		fill(randomH, randomS, randomB);
+
+// 		//Hot[Hot.length] = randomH;
+//         //Soggy[Soggy.length] = randomS;
+//         //Boogers[Boogers.length] = randomB;
+// 	}
+// }
+
+//Custom random function since random() is not accessible outside p5s functions
+function generateRandomNumber(min, max){
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomHue(){
+	return Math.floor(Math.random() * 321);
+}
+
+function getRandomSaturation(){
+	return Math.floor(Math.random() * (100 - 30 + 1) + 30);
+}
+
+function getRandomBrightness(){
+	return Math.floor(Math.random() * (100 - 30 + 1) + 30);
+}
+
+function colorBackground(){
+	//Get random colors for the background
+	let backgroundH = getRandomHue();
+	let backgroundS = getRandomSaturation();
+	let backgroundB = getRandomBrightness();
+	//Add the background values to the HSB arrays
+	H.push(backgroundH);
+	S.push(backgroundS);
+	B.push(backgroundB);
+
+	console.log("Background: " + backgroundH + ", " + backgroundS + ", " + backgroundB);
+}
+
+// function getRandomColor(){
+// 	//Choose 3 random HSB values
+// 	randomH = generateRandomNumber()
+// }
+
+//Arrays for HSB values
+let H = [];
+let S = [];
+let B = [];
+
+
 // put setup code here
 function setup() {
 	//Create canvas
 	var canvas = createCanvas(400, 400);
 	//Get coordinates to place the canvas in the middle of the screen
-	var canvasXPos = (windowWidth - width) / 2; //'windowWidth' and 'windowHeight' store thw width and hwight of the inner window
+	var canvasXPos = (windowWidth - width) / 2; //'windowWidth' and 'windowHeight' store thw width and height of the inner window
 	var canvasYPos = (windowHeight - height) / 2; //The 'width' and' height' variables contain the width and height of the display window as defined by createCanvas()
 	//Place the canvas in the middle of the screen
 	canvas.position(canvasXPos, canvasYPos);
 
-	background(255, 0, 200);
+	//Set the color mode to HSB
+	colorMode(HSB, 360, 100, 100);
+
+	//Get a random background color
+	colorBackground();	
+	//Set the background color
+	background(H[0], S[0], B[0]);
+
+	console.log("BG H: " + H[0] + " BG S: " + S[0] + " BG B: " + B[0]);
 
 
+	//Can clear an array this way
+	// H = [];
+	// console.log(H);
+
+	
+
+	// const circleDiameter = 50;
+	// var xPos = circleDiameter / 2;
+	// var yPos = circleDiameter /2;
+
+	// var diff = ;
+
+	// for(var j = 0; j < 15 ; j++)
+	// {
+	// 	if(j == ((100/diff) - 1))
+	// 	{
+	// 		// stroke(51);
+	// 	}
+
+
+	// 	var hueVal = (diff * j);
+	// 	var randomS = 100;
+	// 	var randomB = 100;
+
+	// 	if(hueVal <= 320)
+	// 	{
+	// 		console.log(j + ": " + hueVal + ", " + randomS + ", " + randomB)
+
+	// 		fill(hueVal, randomS, randomB);
+	// 		circle((xPos * j) + 90 , (yPos * j) + 90, circleDiameter);
+	// 	}
+	// }
 }
 
 // put drawing code here
