6e0261b685090d4a8e708d53f646e84628a47727
anthonyhenry <anthonyh1497@gmail.com>
2022-05-07 02:12:58 -0700

    Cleaned up the code

--- 100/Projects/Skull/sketch.js
+++ 100/Projects/Skull/sketch.js
@@ -1,56 +1,34 @@
-// 9 colors min, 14 max
-// 360/9 = 40
-//
-// H: 0-320 ; Diff: 
-// S: 30-100 ; Diff: 60
-// B: 40-100 ; Diff: 10
-
-// function getRandomColor(){
-// 	//Choose random HSB values
-// 	var randomH = random(0, 320);
-// 	var randomS = random(30, 100);
-// 	var randomB = random(30, 100); change to 40 if needed
-
-// 	const hDifference = 20;
-// 	const sDifference = 30;
-// 	const bDifference = 15;
-
-// 	for(var i = 0; i < H.length; i++)
-// 	{
-// 		fill(randomH, randomS, randomB);
-
-// 		//Hot[Hot.length] = randomH;
-//         //Soggy[Soggy.length] = randomS;
-//         //Boogers[Boogers.length] = randomB;
-// 	}
-// }
-
 //Custom random function since random() is not accessible outside p5s functions
 function generateRandomNumber(min, max){
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function getRandomHue(){
+	//Return a random numnber between 0 and 300 for the hue
 	return Math.floor(Math.random() * 301);
 }
 
-function getRandomSaturation(){
+function getRandomSaturationOrBrightness(){
+	//Return a random numnber between 30 and 100 for the saturation and brightness
 	return Math.floor(Math.random() * (100 - 30 + 1) + 30);
 }
 
-function getRandomBrightness(){
-	return Math.floor(Math.random() * (100 - 30 + 1) + 30);
-}
+//Arrays for HSB values
+let H = [];
+let S = [];
+let B = [];
 
 function colorBackground(){
 	//Get random colors for the background
 	let backgroundH = getRandomHue();
-	let backgroundS = getRandomSaturation();
-	let backgroundB = getRandomBrightness();
+	let backgroundS = getRandomSaturationOrBrightness();
+	let backgroundB = getRandomSaturationOrBrightness();
 	//Add the background values to the HSB arrays
 	H.push(backgroundH);
 	S.push(backgroundS);
 	B.push(backgroundB);
+	//Color the background
+	background(backgroundH, backgroundS, backgroundB);
 
 	console.log("Background: " + backgroundH + ", " + backgroundS + ", " + backgroundB);
 }
@@ -58,8 +36,8 @@ function colorBackground(){
 function getRandomColor(bodypart){
 	//Choose random HSB color values
 	let randomH = getRandomHue();
-	let randomS = getRandomSaturation();
-	let randomB = getRandomBrightness();
+	let randomS = getRandomSaturationOrBrightness();
+	let randomB = getRandomSaturationOrBrightness();
 
 	//Gap values - determines how different each piece of the puzzle must be from a similar color
 	let hGap;
@@ -67,14 +45,14 @@ function getRandomColor(bodypart){
 	const bGap = 15;
 	
 	//Check if the hue is green
-	if(randomH >= 75 && randomH <= 155) //Green is 75-170
+	if(randomH >= 75 && randomH <= 155) //Green is roughly 75-170
 	{
-		//Larger comparison threshold for green colors
+		//Larger hue comparison threshold for green colors
 		hGap = 40;
 	}
 	else
 	{
-		//Regular comparison threshold
+		//Regular hue comparison threshold
 		hGap = 20;
 	}
 
@@ -91,8 +69,9 @@ function getRandomColor(bodypart){
 			{
 				if(sDifference < sGap)
 				{
+					//Colors are too similar, get a new one
 					console.log("FAIL \n" + bodypart + ": " + randomH + ", " + randomS + ", " + randomB);
-					getRandomColor(bodypart);
+					getRandomColor();
 					return;
 				}
 			}
@@ -101,13 +80,12 @@ function getRandomColor(bodypart){
 
 	console.log("SUCCESS \n" + bodypart + ": " + randomH + ", " + randomS + ", " + randomB);
 
+	//Selected a color
 	fill(randomH, randomS, randomB);
 	//Add the new color to the color arrays
 	H.push(randomH);
 	S.push(randomS);
 	B.push(randomB);
-
-
 }
 
 
@@ -152,7 +130,6 @@ function drawSkull(){
 	triangle(200 + noseSpacing, noseTop, 200 + noseSpacing, noseTop + noseHeight, 200 + noseSpacing + noseWidth, noseTop + noseHeight);//Right nostril
 
 	//Teeth
-	//stroke(0, 0, 0);//black stroke
 	strokeWeight(2.8);
 	var teethWidth = 24;
 	var teethHeight = 49;
@@ -160,7 +137,6 @@ function drawSkull(){
 	var teethYPos = jawLength - teethHeight * 1.5;
 	var toothRoundness = 300;
 	//Teeth loop
-	//fill(random(0, 255), random(0,255), random(0, 255));
 	getRandomColor();
 	stroke(0, 0, 0);
 	rect(200 - teethWidth/2, teethYPos, teethWidth, teethHeight, toothRoundness);
@@ -178,12 +154,6 @@ function drawSkull(){
 	line(200 + teethWidth * 2 + teethWidth/2, teethYPos + teethHeight/2, 200 - teethWidth * 2 - teethWidth/2, teethYPos + teethHeight/2);
 }
 
-//Arrays for HSB values
-let H = [];
-let S = [];
-let B = [];
-
-
 // put setup code here
 function setup() {
 	//Create canvas
@@ -197,51 +167,21 @@ function setup() {
 	//Set the color mode to HSB
 	colorMode(HSB, 360, 100, 100);
 
-	//Get a random background color
-	colorBackground();	
-	//Set the background color
-	background(H[0], S[0], B[0]);
-
+	//Color the background
+	colorBackground();
+	
+	//Draw the skull
 	drawSkull();
 
 
 	//Can clear an array this way
 	// H = [];
 	// console.log(H);
-
-	
-
-	// const circleDiameter = 50;
-	// var xPos = circleDiameter / 2;
-	// var yPos = circleDiameter /2;
-
-	// var diff = ;
-
-	// for(var j = 0; j < 15 ; j++)
-	// {
-	// 	if(j == ((100/diff) - 1))
-	// 	{
-	// 		// stroke(51);
-	// 	}
-
-
-	// 	var hueVal = (diff * j);
-	// 	var randomS = 100;
-	// 	var randomB = 100;
-
-	// 	if(hueVal <= 320)
-	// 	{
-	// 		console.log(j + ": " + hueVal + ", " + randomS + ", " + randomB)
-
-	// 		fill(hueVal, randomS, randomB);
-	// 		circle((xPos * j) + 90 , (yPos * j) + 90, circleDiameter);
-	// 	}
-	// }
 }
 
 // put drawing code here
 function draw() {
-
+	
 }
 
 //p5 examples: https://p5js.org/examples/
