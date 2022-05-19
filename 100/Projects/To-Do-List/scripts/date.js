6b9ca7cf2a2c21879422d5acdb59dd47b4002b86
anthonyhenry <anthonyh1497@gmail.com>
2022-05-18 23:45:40 -0700

    Verify new task entry

--- 100/Projects/To-Do-List/scripts/date.js
+++ 100/Projects/To-Do-List/scripts/date.js
@@ -17,3 +17,33 @@ currentDate = mm + "/" + dd + "/" + yyyy;
 
 //Display the date in the top right corner
 document.querySelector("#date").innerHTML = currentDate;
+
+document.querySelector("#todoForm").onsubmit = function(){
+	let description = document.querySelector("#newTaskDescription").value.trim();
+	let deadline = document.querySelector("#newTaskDueDate").value;
+
+
+	console.log(description);
+	console.log(deadline);
+
+	//Verify entry
+	if(description == "")
+	{
+		alert("Please provide a title or description for your new to do item.")
+	}
+
+	//Checek for any [ or ] in the new entry
+
+	else if(date == "")
+	{
+		alert("Please provide a due date for your new to do item")
+	}
+	//Entry verified
+	else
+	{
+
+	}
+
+	console.log("test");
+	return false;
+}
