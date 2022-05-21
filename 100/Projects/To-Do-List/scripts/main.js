dc82666ed64a50d80108d0f5aab4bcf257ff3623
anthonyhenry <anthonyh1497@gmail.com>
2022-05-21 01:26:32 -0700

    Small changes in prep for bigger ones

---
+++ 100/Projects/To-Do-List/scripts/main.js
@@ -0,0 +1,106 @@
+//Get and format today's date
+let todaysDate = new Date();
+let dd = todaysDate.getDate();
+let mm = todaysDate.getMonth() + 1; //January is 0!
+let yyyy = todaysDate.getFullYear();
+
+if(dd < 10)
+{
+	dd = "0" + dd;
+}
+if(mm < 10)
+{
+	mm = "0" + mm;
+}
+
+//set date as month/day/year
+let todaysDateFormatted = mm + "/" + dd + "/" + yyyy;
+
+//Display the date in the top right corner
+document.querySelector("#date").innerHTML = todaysDateFormatted;
+
+//New task form submission
+document.querySelector("#newTaskForm").onsubmit = function(){
+	let description = document.querySelector("#newTaskDescription").value.trim();
+	let deadline = document.querySelector("#newTaskDueDate").value;
+
+	//Verify entry
+	if(/^$/.test(description))
+	{
+		alert("Please provide a title or description for your new to do item.")
+	}
+	//Checek for any [ or ] in the new entry
+	else if(/[\[\]]/.test(description))
+	{
+		alert("Please do not use '[' or ']' characters in your new to do item.");
+	}
+	else if(/^$/.test(deadline))
+	{
+		alert("Please provide a due date for your new to do item")
+	}
+	//Entry verified
+	else
+	{
+
+
+
+
+		// let test = new taskItem(description, deadline);
+		// console.log(test.calculatePriority());
+	}
+	return false;
+}
+
+class taskItem{
+	constructor(description, deadline){
+		this.description = description;
+		this.deadline = deadline;
+	}
+
+	calculatePriority(){
+		//Format the deadline
+		let formattedDeadline = this.deadline.split("-");
+		formattedDeadline = formattedDeadline[1]+ "/" + formattedDeadline[2] + "/" + formattedDeadline[0];
+
+		const MILLISECONDS_IN_A_DAY = 86400000;
+		
+		//Create a date variable for comparing
+		let daysRemaing = ( (new Date(formattedDeadline) - new Date(todaysDateFormatted)) / MILLISECONDS_IN_A_DAY);
+
+		if(daysRemaing < 0)
+		{
+			return "Overdue";
+		}
+		else if(daysRemaing == 0)
+		{
+			return "Highest";
+		}
+		else if(daysRemaing <= 3)
+		{
+			return "High";
+		}
+		else if(daysRemaing < 7)
+		{
+			return "Medium";
+		}
+		else
+		{
+			return "Low";
+		}
+	}
+}
+
+
+const paragraph = 'The quick brown fox jumps over the lazy dog [10-12-2022]';
+const regex = /\[(.*?)\]/;
+const found = paragraph.match(regex)[1];
+
+console.log(found);
+
+// const startDate = new Date("4/18/2022");
+
+// const endDate = new Date("4/18/2022");
+
+// const dayGap = (endDate - startDate) / 86400000
+
+// console.log(dayGap);
