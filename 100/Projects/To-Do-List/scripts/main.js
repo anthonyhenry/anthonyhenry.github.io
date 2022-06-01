5ac3145e552d707d2c045ad1e2fb5e66b4365351
anthonyhenry <anthonyh1497@gmail.com>
2022-06-01 00:37:04 -0700

    First Pass at Updating Priorities

--- 100/Projects/To-Do-List/scripts/main.js
+++ 100/Projects/To-Do-List/scripts/main.js
@@ -20,6 +20,78 @@ let todaysDateFormatted = mm + "/" + dd + "/" + yyyy;
 //Display the date in the top right corner
 document.querySelector("#date").innerHTML = todaysDateFormatted;
 
+//Allow all task to be deleted
+bindDelete();
+
+function bindDelete(){
+	//Create an array of all delete buttons
+	let deleteButtons = document.querySelectorAll(".remove");
+
+	//Loop through all the delete buttons
+	for(let j = 0; j < deleteButtons.length; j++)
+	{
+		//Add a function for deleting tasks to each delete button
+		deleteButtons[j].onclick = function(){
+			//Find which list the task belongs to
+			let parentList = this.parentNode.parentNode.parentNode;
+			//Grab the task to delete
+			let taskToRemove = this.parentNode.parentNode;
+
+			//Delete the task
+			parentList.removeChild(taskToRemove);
+		}
+	}
+}
+
+//Update Priorities
+let tasksDueToday = document.querySelector("#tasksDueToday").children;
+let highPriorityTasks = document.querySelector("#highPriorityTasks").children;
+let mediumPriorityTasks = document.querySelector("#mediumPriorityTasks").children;
+let lowPriorityTasks = document.querySelector("#lowPriorityTasks").children;
+
+updatePriorities(tasksDueToday);
+updatePriorities(highPriorityTasks);
+updatePriorities(mediumPriorityTasks);
+updatePriorities(lowPriorityTasks);
+
+function updatePriorities(tasksArray){
+	//For loop to check each task in the list
+	for(let i = 0; i < tasksArray.length; i++)
+	{
+		//Skip Examples
+		if(tasksArray[i].classList.contains("example") == false) //For some reason ! and == false behave differently
+		{
+			//Get the due date for the current task
+			let taskDeadline = getTaskDueDate(tasksArray[i]);
+			//Get the task priority on load
+			let currentTaskPriority = tasksArray[i].children[1].children[0].innerText;
+			//Get the updated priority
+			let newPriority = calculatePriority(taskDeadline)
+
+			//Compare current task priority with calculated priority
+			if(currentTaskPriority != newPriority)
+			{
+				formatTask(tasksArray[i], newPriority);
+				placeTaskInList2(getListToPlaceTask(newPriority), tasksArray[i]);
+
+				// if(newPriority == "Overdue")
+				// {
+				// 	placeTaskInList2(getListToPlaceTask(newPriority), tasksArray[i]);
+				// }
+			}
+		}
+	}
+}
+
+function getTaskDueDate(task){
+	//Get the due date of the current task in the list
+	let dueDate = task.children[0].children[0].innerText;
+	const dateRegex = /\[(.*?)\]/;
+	dueDate = dueDate.match(dateRegex)[1];
+	dueDate = new Date(dueDate);
+	return dueDate;
+}
+
 //New task form submission
 document.querySelector("#newTaskForm").onsubmit = function(){
 	let description = document.querySelector("#newTaskDescription").value.trim();
@@ -153,7 +225,67 @@ function calculatePriority(dueDate){
 
 }
 
-function placeTaskInList(list, taskElement, taskToPlaceDueDate){
+function formatTask(task, priority){
+	let taskLi = task;
+	let taskDescriptionDiv = task.children[0];
+	let taskBadgeSpan = task.children[1].children[0];
+
+	//Remove any classes that might carry over from lower priorities moving up
+	taskLi.classList.remove("bg-warning");
+	taskBadgeSpan.classList.remove("bg-dark", "bg-danger", "bg-warning", "text-dark", "bg-success");
+
+	if(priority == "Overdue")
+	{
+		taskLi.classList.add("bg-dark");
+		taskDescriptionDiv.classList.add("text-white");
+		taskBadgeSpan.classList.add("bg-white", "text-dark");
+	}
+	else if(priority == "Due Today")
+	{
+		taskLi.classList.add("bg-warning");
+		taskBadgeSpan.classList.add("bg-dark");
+	}
+	else if(priority == "High")
+	{
+		taskBadgeSpan.classList.add("bg-danger");
+	}
+	else if(priority == "Medium")
+	{
+		taskBadgeSpan.classList.add("bg-warning", "text-dark");
+	}
+	else
+	{
+		taskBadgeSpan.classList.add("bg-success");
+	}
+
+	//Set the priority text
+	taskBadgeSpan.innerText = priority;
+}
+
+function getListToPlaceTask(priority){
+	if(priority == "Overdue")
+	{
+		return document.querySelector("#overdueTasks");
+	}
+	else if(priority == "Due Today")
+	{
+		return document.querySelector("#tasksDueToday");
+	}
+	else if(priority == "High")
+	{
+		return document.querySelector("#highPriorityTasks");
+	}
+	else if(priority == "Medium")
+	{
+		return document.querySelector("#mediumPriorityTasks");
+	}
+	else
+	{
+		return document.querySelector("#lowPriorityTasks");
+	}
+}
+
+function placeTaskInList2(list, taskElement){
 	//Check if there are any items currently in the list
 	if(list.children.length > 0)
 	{
@@ -177,12 +309,11 @@ function placeTaskInList(list, taskElement, taskToPlaceDueDate){
 			for(let i = 0; i < listContents.length; i++)
 			{
 				//Get the due date of the current task in the list
-				let dueDateToCompare = listContents[i].children[0].children[0].innerText;
-				const dateRegex = /\[(.*?)\]/;
-				dueDateToCompare = dueDateToCompare.match(dateRegex)[1];
-				dueDateToCompare = new Date(dueDateToCompare);
+				let dueDateToCompare = getTaskDueDate(listContents[i]);
 
+				
 				//Format the date of the task to place
+				let taskToPlaceDueDate = getTaskDueDate(taskElement);
 				taskToPlaceDueDate = new Date(taskToPlaceDueDate);
 
 				//Check if the new task due date comes before the current task in the list
@@ -209,25 +340,62 @@ function placeTaskInList(list, taskElement, taskToPlaceDueDate){
 	}
 }
 
-//Allow all task to be deleted
-bindDelete();
-
-function bindDelete(){
-	//Create an array of all delete buttons
-	let deleteButtons = document.querySelectorAll(".remove");
-
-	//Loop through all the delete buttons
-	for(let j = 0; j < deleteButtons.length; j++)
+function placeTaskInList(list, taskElement, taskToPlaceDueDate){
+	//Check if there are any items currently in the list
+	if(list.children.length > 0)
 	{
-		//Add a function for deleting tasks to each delete button
-		deleteButtons[j].onclick = function(){
-			//Find which list the task belongs to
-			let parentList = this.parentNode.parentNode.parentNode;
-			//Grab the task to delete
-			let taskToRemove = this.parentNode.parentNode;
+		//Check if the first item in the list is an example
+		let firstListItem = list.children[0];
 
-			//Delete the task
-			parentList.removeChild(taskToRemove);
+		if(firstListItem.classList.contains("example"))
+		{
+			//Delete the example
+			list.removeChild(firstListItem);
+			//Add the new task
+			list.appendChild(taskElement);
+			return;
 		}
+		else
+		{
+			//Array for all the tasks in a list
+			let listContents = list.children;
+
+			//Loop through all the list tasks
+			for(let i = 0; i < listContents.length; i++)
+			{
+				//Get the due date of the current task in the list
+				let dueDateToCompare = getTaskDueDate(listContents[i]);
+
+				// listContents[i].children[0].children[0].innerText;	//Make this a function since it will be used in Update Priorities
+				// const dateRegex = /\[(.*?)\]/;
+				// dueDateToCompare = dueDateToCompare.match(dateRegex)[1];
+				// dueDateToCompare = new Date(dueDateToCompare);
+				// console.log(dueDateToCompare);
+				// console.log(getTaskDueDate(listContents[i]));
+
+				//Format the date of the task to place
+				taskToPlaceDueDate = new Date(taskToPlaceDueDate);
+
+				//Check if the new task due date comes before the current task in the list
+				if(dueDateToCompare > taskToPlaceDueDate)
+				{
+					//Add the new task before this task
+					list.insertBefore(taskElement, listContents[i]);
+					return;
+				}
+				//Check if all the tasks in the list have been checked
+				else if(i == (listContents.length -1) )
+				{
+					//Add the new task as the last item in the list
+					list.appendChild(taskElement);
+					return
+				}
+			}
+		}
+	}
+	else
+	{
+		//Add the new task as the first item in the list
+		list.appendChild(taskElement);
 	}
 }
