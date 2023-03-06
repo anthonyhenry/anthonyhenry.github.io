0ecda6cc9faa32757feb2f45649296a07962a6d0
anthonyhenry <anthonyh1497@gmail.com>
2023-03-05 22:57:34 -0800

    Working embed and youtube link

--- 100/Projects/Pomodoro-Task-Manager/scripts/jukebox.js
+++ 100/Projects/Pomodoro-Task-Manager/scripts/jukebox.js
@@ -1,36 +1,3 @@
-// let player = document.querySelector("iframe");
-
-// console.log(player);
-
-
-
-// document.querySelector("#jukeboxForm").onsubmit = function(){
-//     let newVideo = document.querySelector("#embedCode").value.trim();
-
-//     //update this to match regular youtube links and turn them into embed codes
-//     var ytRegEx = /^<iframe.*src.*youtube.*embed.*<\/iframe>$/;
-
-//     console.log(ytRegEx.test(newVideo));
-
-
-//     //
-//     if(!ytRegEx.test(newVideo) || /^$/.test(newVideo))
-//     {
-//         alert("Error: The input you sumbited does not appear to be a Youtube embed code. Please paste a youtube embed code into the form and resubmit.");
-//     }
-//     else
-//     {
-//         console.log(document.querySelector("#jukebox").children);
-//         document.querySelector("#jukebox").children[0] = newVideo;
-
-//         // player.innerHTML = newVideo;
-        
-//     }
-
-//     return false;
-// }
-
-
 //New youtube embed code submitted
 document.querySelector("#jukeboxForm").onsubmit = function(){
     //Get the new embed code that has been submitted
@@ -39,9 +6,13 @@ document.querySelector("#jukeboxForm").onsubmit = function(){
     const ytEmbedLinkRegEx = /^<iframe.*src.*youtube.*embed.*<\/iframe>$/;
     
     // Verify the submitted embed code
-    if( !getYouTubeVideoIdByUrl(newVideoEmbedCode) && ytEmbedLinkRegEx.test(newVideoEmbedCode) == false )
+    if(/^$/.test(newVideoEmbedCode))
     {
-        alert("Error: The input you submitted does not appear to be a Youtube embed code. Please paste a youtube embed code into the form and resubmit.");
+        alert("Please provide a YouTube embed code.")
+    }
+    else if( !getYouTubeVideoIdByUrl(newVideoEmbedCode) && ytEmbedLinkRegEx.test(newVideoEmbedCode) == false )
+    {
+        alert("Error: The input you submitted does not appear to be a YouTube embed code. Please paste a YouTube embed code into the form and resubmit. \nTo get the embed code for a YouTube video follow these steps: \n\n 1. Go to YouTube and find the video you would like to embed\n 2. Click on the Share button below the video\n 3. Select the Embed option that appears.\n 4.You will see a box with some code inside. This code is the embed code for the video. Copy that code.\n 5.Paste the code into the YouTube Embed Code input box on this website.");
     }
     // User supplied propper embed code
     else if(ytEmbedLinkRegEx.test(newVideoEmbedCode))
@@ -53,43 +24,30 @@ document.querySelector("#jukeboxForm").onsubmit = function(){
         embeddedYoutubeVid.firstElementChild.width = "100%"; //Prevent the player from going outside the window
         embeddedYoutubeVid.firstElementChild.height = "100%"; //Auto size height based on width
     }
+    //User supplied youtube link but not an embed code
+    else
+    {
+        //Get the youtube video's id
+        let videoID = getYouTubeVideoIdByUrl(newVideoEmbedCode);
+
+        //Get the iframe element
+        let player = document.querySelector("iframe");
+        //Split the iframe src attibute into parts
+        let playerSrc = player.src.split("/");
+        //Change the last part of the src to the new video id
+        playerSrc[playerSrc.length-1] = videoID
+        //Recombine the src url
+        let newSrc = playerSrc.join("/");
+        //Set the embedded video source to the new video and clear the title
+        player.src = newSrc;
+        player.title = "";
+    }
 
     //Reset form
     document.querySelector("#embedCode").value = "";
 
-    
-
-        
-
-    // //User supplied youtube link but not an embed code
-    // else if(getYouTubeVideoIdByUrl(newVideoEmbedCode) != null)
-    // {
-    //     //Get the youtube video's id
-    //     let videoID = getYouTubeVideoIdByUrl(newVideoEmbedCode);
-
-        
-    //     //Split the iframe src attibute into parts
-    //     let playerSrc = player.src.split("/");
-    //     //Change the last part of the src to the new video id
-    //     playerSrc[playerSrc.length-1] = videoID
-    //     //Recombine the src url
-    //     let newSrc = playerSrc.join("/");
-    //     //Set the embedded video source to the new video and clear the title
-    //     player.src = newSrc;
-    //     player.title = "";
-    // }
-    // else
-    // {
-    //     alert("Fail");
-    // }
-
     return false;
 }
-
-console.log(getYouTubeVideoIdByUrl('<iframe width="950" height="534" src="https://www.youtube.com/embed/-dTHqO1dN3M" title="60 Minutes of Relaxing/Emotional Once Upon a Time Music" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'));
-console.log(getYouTubeVideoIdByUrl('https://www.youtube.com/watch?v=-dTHqO1dN3M'));
-console.log(getYouTubeVideoIdByUrl('<iframe width="950" height="534" src="https://www.youtube.com/embed/jzF_y039slk?list=PLXh3_htGAZzmLFV3zqZLchUbHI_llE_7f" title="Over the Rainbow/Simple Gifts (Piano/Cello Cover) - The Piano Guys" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'));
-console.log(getYouTubeVideoIdByUrl('https://www.youtube.com/watch?v=mJ_fkw5j-t0&list=PLXh3_htGAZzmLFV3zqZLchUbHI_llE_7f&index=1'));
 
 function getYouTubeVideoIdByUrl(url) {
     const reg = /^(https?:)?(\/\/)?((www\.|m\.)?youtube(-nocookie)?\.com\/((watch)?\?(feature=\w*&)?vi?=|embed\/|vi?\/|e\/)|youtu.be\/)([\w\-]{10,20})/i
@@ -100,11 +58,3 @@ function getYouTubeVideoIdByUrl(url) {
         return null;
     }
 }
-
-// ^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(\?\S*)?$ 
-// /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/
-
-// <iframe width="950" height="534" src="https://www.youtube.com/embed/-dTHqO1dN3M" title="60 Minutes of Relaxing/Emotional Once Upon a Time Music" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-// https://www.youtube.com/watch?v=-dTHqO1dN3M
-// <iframe width="950" height="534" src="https://www.youtube.com/embed/jzF_y039slk?list=PLXh3_htGAZzmLFV3zqZLchUbHI_llE_7f" title="Over the Rainbow/Simple Gifts (Piano/Cello Cover) - The Piano Guys" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-// https://youtu.be/jzF_y039slk
