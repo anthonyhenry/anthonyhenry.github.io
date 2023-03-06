2fe250095c2de3a98716c994f423ef8b014813da
anthonyhenry <anthonyh1497@gmail.com>
2023-03-06 01:20:39 -0800

    Best version
    Best implementation of Jukebox feature that I am happy with for now. Will need to update to save the chosen video for next time, and also for different positioning based on screen size

--- 100/Projects/Pomodoro-Task-Manager/scripts/jukebox.js
+++ 100/Projects/Pomodoro-Task-Manager/scripts/jukebox.js
@@ -1,15 +1,15 @@
 document.querySelector("#jukeboxForm").onsubmit = function(){
     //Get a reference to the new embed code that has been submitted
-    const newVideoEmbedCode = document.querySelector("#embedCode").value.trim();
+    const newEmbeddedVideoCode = document.querySelector("#embedCode").value.trim();
     //Youtube embed code regex
     const ytEmbedLinkRegEx = /^<iframe.*src.*youtube.*embed.*<\/iframe>$/;
     
     // Verify the submitted embed code
-    if(/^$/.test(newVideoEmbedCode))
+    if(/^$/.test(newEmbeddedVideoCode))
     {
         alert("Please provide a YouTube embed code.")
     }
-    else if( !getYouTubeVideoIdByUrl(newVideoEmbedCode) && !ytEmbedLinkRegEx.test(newVideoEmbedCode))
+    else if( !getYouTubeVideoIdByUrl(newEmbeddedVideoCode) && !ytEmbedLinkRegEx.test(newEmbeddedVideoCode))
     {
         alert("Error: The input you submitted does not appear to be a YouTube embed code. Please paste a YouTube embed code into the form and resubmit. \nTo get the embed code for a YouTube video follow these steps: \n\n 1. Go to YouTube and find the video you would like to embed\n 2. Click on the Share button below the video\n 3. Select the Embed option that appears.\n 4.You will see a box with some code inside. This code is the embed code for the video. Copy that code.\n 5.Paste the code into the YouTube Embed Code input box on this website.");
     }
@@ -17,25 +17,25 @@ document.querySelector("#jukeboxForm").onsubmit = function(){
     else
     {
         //Set variables for the new embeded video's source and title
-        let newVideoEmbedSource = newVideoEmbedCode;
-        let newVideoEmbedTitle = "Embedded YouTube Video";
+        let newEmbeddedVideoSource = newEmbeddedVideoCode; //this will get reassigned later
+        let newEmbeddedVideoTitle = "Embedded YouTube Video"; //this might get reassigned later
 
         //Check if the user supplied a proper embed code
-        if(ytEmbedLinkRegEx.test(newVideoEmbedCode))
+        if(ytEmbedLinkRegEx.test(newEmbeddedVideoCode))
         {
-            //Get just the source of the embeded video and save it in the newVideoEmbedSource variable
+            //Get just the source of the embeded video and save it in the newEmbeddedVideoSource variable
             const regGetEmbedSource = /src="([^"]*)"/;
-            newVideoEmbedSource = newVideoEmbedCode.match(regGetEmbedSource);
-            newVideoEmbedSource = newVideoEmbedSource[1];
+            newEmbeddedVideoSource = newEmbeddedVideoCode.match(regGetEmbedSource);
+            newEmbeddedVideoSource = newEmbeddedVideoSource[1];
 
             //Set the new title for the embedded youtube video
             const regGetEmbedTitle = /title="([^"]*)"/;
-            newVideoEmbedTitle = newVideoEmbedCode.match(regGetEmbedTitle);
-            newVideoEmbedTitle = newVideoEmbedTitle[1];
+            newEmbeddedVideoTitle = newEmbeddedVideoCode.match(regGetEmbedTitle);
+            newEmbeddedVideoTitle = newEmbeddedVideoTitle[1];
         }
 
-        //Get the youtube video source's id
-        let videoID = getYouTubeVideoIdByUrl(newVideoEmbedSource);
+        //Get the youtube video's id
+        let videoID = getYouTubeVideoIdByUrl(newEmbeddedVideoSource);
 
         //Get a reference to the iframe element
         let player = this.previousElementSibling;
@@ -45,9 +45,9 @@ document.querySelector("#jukeboxForm").onsubmit = function(){
         playerSrc[playerSrc.length-1] = videoID
         //Recombine the src url
         let newSrc = playerSrc.join("/");
-        //Set the embedded video source to the new video and clear the title
+        //Set the embedded video source and title attributes
         player.src = newSrc;
-        player.title = newVideoEmbedTitle;
+        player.title = newEmbeddedVideoTitle;
     }
 
     //Reset form
