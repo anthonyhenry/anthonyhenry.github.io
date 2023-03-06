0b25ace31b118906868265eb00c26a448510ed50
anthonyhenry <anthonyh1497@gmail.com>
2023-03-06 00:11:46 -0800

    Cleaning up some code

--- 100/Projects/Pomodoro-Task-Manager/scripts/jukebox.js
+++ 100/Projects/Pomodoro-Task-Manager/scripts/jukebox.js
@@ -1,6 +1,6 @@
 //New youtube embed code submitted
 document.querySelector("#jukeboxForm").onsubmit = function(){
-    //Get the new embed code that has been submitted
+    //Get a reference to the new embed code that has been submitted
     let newVideoEmbedCode = document.querySelector("#embedCode").value.trim();
     //Youtube embed code regex
     const ytEmbedLinkRegEx = /^<iframe.*src.*youtube.*embed.*<\/iframe>$/;
@@ -10,29 +10,29 @@ document.querySelector("#jukeboxForm").onsubmit = function(){
     {
         alert("Please provide a YouTube embed code.")
     }
-    else if( !getYouTubeVideoIdByUrl(newVideoEmbedCode) && ytEmbedLinkRegEx.test(newVideoEmbedCode) == false )
+    else if( !getYouTubeVideoIdByUrl(newVideoEmbedCode) && !ytEmbedLinkRegEx.test(newVideoEmbedCode))
     {
         alert("Error: The input you submitted does not appear to be a YouTube embed code. Please paste a YouTube embed code into the form and resubmit. \nTo get the embed code for a YouTube video follow these steps: \n\n 1. Go to YouTube and find the video you would like to embed\n 2. Click on the Share button below the video\n 3. Select the Embed option that appears.\n 4.You will see a box with some code inside. This code is the embed code for the video. Copy that code.\n 5.Paste the code into the YouTube Embed Code input box on this website.");
     }
-    // User supplied propper embed code
-    else if(ytEmbedLinkRegEx.test(newVideoEmbedCode))
-    {
-        //Update the embedded video
-        let embeddedYoutubeVid = document.querySelector("#embeddedVideo");
-        document.querySelector("#embeddedVideo").innerHTML = newVideoEmbedCode;
-        // Size the new embedded video
-        embeddedYoutubeVid.firstElementChild.width = "100%"; //Prevent the player from going outside the window
-        embeddedYoutubeVid.firstElementChild.height = "100%"; //Auto size height based on width
-    }
-    //User supplied youtube link but not an embed code
+    //User supplied an embed code or youtube video link
     else
     {
-        //Get the youtube video's id
+        //Get a reference to the iframe element
+        let player = this.previousElementSibling;
+
+        //Check if the user supplied a proper embed code
+        if(ytEmbedLinkRegEx.test(newVideoEmbedCode))
+        {
+            //Get just the source of the embeded video and save it in the existing newVideoEmbedCode variable
+            const regGetEmbedSource = /src="([^"]*)"/;
+            newVideoEmbedCode = newVideoEmbedCode.match(regGetEmbedSource);
+            newVideoEmbedCode = newVideoEmbedCode[1];
+        }
+
+        //Get the youtube video source's id
         let videoID = getYouTubeVideoIdByUrl(newVideoEmbedCode);
 
-        //Get the iframe element
-        let player = document.querySelector("iframe");
-        //Split the iframe src attibute into parts
+        ////Split the iframe src attibute into parts
         let playerSrc = player.src.split("/");
         //Change the last part of the src to the new video id
         playerSrc[playerSrc.length-1] = videoID
