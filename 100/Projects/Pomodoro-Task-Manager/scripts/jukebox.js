613e323169495c2e05fa0b48e7b392d688ac0e26
anthonyhenry <anthonyh1497@gmail.com>
2023-03-05 21:49:36 -0800

    Second Pass at Embedding new videos with embed code
    Better version of embedding a new video when a proper embed code has been supplied

--- 100/Projects/Pomodoro-Task-Manager/scripts/jukebox.js
+++ 100/Projects/Pomodoro-Task-Manager/scripts/jukebox.js
@@ -2,9 +2,7 @@
 
 // console.log(player);
 
-// //Size the embedded video
-// player.width = "100%"; //Prevent the player from going outside the window
-// player.height = ""; //Auto size height based on width
+
 
 // document.querySelector("#jukeboxForm").onsubmit = function(){
 //     let newVideo = document.querySelector("#embedCode").value.trim();
@@ -32,44 +30,58 @@
 //     return false;
 // }
 
-//New youtube embed code submission
+
+//New youtube embed code submitted
 document.querySelector("#jukeboxForm").onsubmit = function(){
     //Get the new embed code that has been submitted
     let newVideoEmbedCode = document.querySelector("#embedCode").value.trim();
-    //Get the embedded video element
-    let embeddedVideoElement = document.querySelector("#embeddedVideo");
-
+    //Youtube embed code regex
     const ytEmbedLinkRegEx = /^<iframe.*src.*youtube.*embed.*<\/iframe>$/;
-
-    //User supplied propper embed code
-    if(ytEmbedLinkRegEx.test(newVideoEmbedCode))
+    
+    // Verify the submitted embed code
+    if( !getYouTubeVideoIdByUrl(newVideoEmbedCode) && ytEmbedLinkRegEx.test(newVideoEmbedCode) == false )
     {
-        //Update embedded video
+        alert("Error: The input you submitted does not appear to be a Youtube embed code. Please paste a youtube embed code into the form and resubmit.");
+    }
+    // User supplied propper embed code
+    else if(ytEmbedLinkRegEx.test(newVideoEmbedCode))
+    {
+        //Update the embedded video
+        let embeddedYoutubeVid = document.querySelector("#embeddedVideo");
         document.querySelector("#embeddedVideo").innerHTML = newVideoEmbedCode;
+        // Size the new embedded video
+        embeddedYoutubeVid.firstElementChild.width = "100%"; //Prevent the player from going outside the window
+        embeddedYoutubeVid.firstElementChild.height = "100%"; //Auto size height based on width
+    }
 
-    }
-    //User supplied youtube link but not an embed code
-    else if(getYouTubeVideoIdByUrl(newVideoEmbedCode) != null)
-    {
-        //Get the youtube video's id
-        let videoID = getYouTubeVideoIdByUrl(newVideoEmbedCode);
+    //Reset form
+    document.querySelector("#embedCode").value = "";
 
-        //Get the iframe element
-        let player = document.querySelector("iframe");
-        //Split the iframe src attibute into parts
-        let playerSrc = player.src.split("/");
-        //Change the last part of the src to the new video id
-        playerSrc[playerSrc.length-1] = videoID
-        //Recombine the src url
-        let newSrc = playerSrc.join("/");
-        //Set the embedded video source to the new video and clear the title
-        player.src = newSrc;
-        player.title = "";
-    }
-    else
-    {
-        alert("Fail");
-    }
+    
+
+        
+
+    // //User supplied youtube link but not an embed code
+    // else if(getYouTubeVideoIdByUrl(newVideoEmbedCode) != null)
+    // {
+    //     //Get the youtube video's id
+    //     let videoID = getYouTubeVideoIdByUrl(newVideoEmbedCode);
+
+        
+    //     //Split the iframe src attibute into parts
+    //     let playerSrc = player.src.split("/");
+    //     //Change the last part of the src to the new video id
+    //     playerSrc[playerSrc.length-1] = videoID
+    //     //Recombine the src url
+    //     let newSrc = playerSrc.join("/");
+    //     //Set the embedded video source to the new video and clear the title
+    //     player.src = newSrc;
+    //     player.title = "";
+    // }
+    // else
+    // {
+    //     alert("Fail");
+    // }
 
     return false;
 }
@@ -78,10 +90,6 @@ console.log(getYouTubeVideoIdByUrl('<iframe width="950" height="534" src="https:
 console.log(getYouTubeVideoIdByUrl('https://www.youtube.com/watch?v=-dTHqO1dN3M'));
 console.log(getYouTubeVideoIdByUrl('<iframe width="950" height="534" src="https://www.youtube.com/embed/jzF_y039slk?list=PLXh3_htGAZzmLFV3zqZLchUbHI_llE_7f" title="Over the Rainbow/Simple Gifts (Piano/Cello Cover) - The Piano Guys" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'));
 console.log(getYouTubeVideoIdByUrl('https://www.youtube.com/watch?v=mJ_fkw5j-t0&list=PLXh3_htGAZzmLFV3zqZLchUbHI_llE_7f&index=1'));
-
-function updateEmbeddedVideo(){
-
-}
 
 function getYouTubeVideoIdByUrl(url) {
     const reg = /^(https?:)?(\/\/)?((www\.|m\.)?youtube(-nocookie)?\.com\/((watch)?\?(feature=\w*&)?vi?=|embed\/|vi?\/|e\/)|youtu.be\/)([\w\-]{10,20})/i
