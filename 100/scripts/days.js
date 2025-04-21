0f317a1a6ba4d9c8335b0d6545cff1b5e8bb902a
anthonyhenry <anthonyh1497@gmail.com>
2025-04-20 21:39:01 -0700

    Removed old files and moved 100 days of code files to 100 folder

--- 100/scripts/days.js
+++ 100/scripts/days.js
@@ -1,30 +1,35 @@
-const startDate = new Date("4/18/2022");
+// Start of challenge
+const START_DATE = new Date("4/15/2025")
+// How many days the challenge will be
+const CHALLENGE_DURATION = 100;
+// Milliseconds in a day
+const MILLISECONDS_IN_A_DAY = 1000 * 60 * 60 * 24; // 1000 milliseconds/second * 60 seconds/minute, 60 minutes/hour * 24 hours/day
+// Find out what the date will be 99 days from the start
+const END_DATE = addDaysToDate(START_DATE, CHALLENGE_DURATION - 1); // Subtract 1 from challenge duration because the first day counts
+// Today's Date
+const TODAY = new Date();
 
-const endDate = new Date("7/26/2022");
-
-const todaysDate = new Date();
-
-//Check if the 100 days have ended
-if(todaysDate > endDate)
+// Display a date range if the 100 days have ended
+if(TODAY > END_DATE)
 {
-	let output = formatDate(startDate) + " - " + formatDate(endDate);
-	
-	document.querySelector("#days").innerHTML = output;
+    const DATE_RANGE = formatDate(START_DATE) + " - " + formatDate(END_DATE);
+    document.querySelector("#days").innerHTML = DATE_RANGE;
 }
-//If the 100 days has not passed, display the remaining days
+// Display remaining days if challenge in progress
 else
 {
-	const diffTime = Math.abs(todaysDate - endDate);
-
-	let output = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
-	document.querySelector("#countdown").innerHTML = output;
+    // Subtract two dates to get milliseconds between the two days
+    let daysRemaining = END_DATE - TODAY;
+    // Convert milliseconds into days
+    daysRemaining = Math.ceil(daysRemaining / MILLISECONDS_IN_A_DAY);
+    
+    document.querySelector("#countdown").innerText = daysRemaining;
 }
 
-function formatDate(dateToFormat){
-	let day = dateToFormat.getDate();
+function formatDate(dateToFormat)
+{
+    let day = dateToFormat.getDate();
 	let month = dateToFormat.getMonth() + 1; //January is 0!
-	let year = dateToFormat.getFullYear();
 
 	if (day < 10)
 	{
@@ -35,7 +40,15 @@ function formatDate(dateToFormat){
 		month = "0" + month;
 	}
 
-	const formattedDate = month + "/" + day + "/" + year;
+    return month + "/" + day + "/" + dateToFormat.getFullYear();
+}
 
-	return formattedDate;
+function addDaysToDate(startDate, numDaysToAdd)
+{
+    // Get the number of milliseconds for this date since the epoch (midnight at the beginning of January 1, 1970)
+    let endDate = startDate.getTime();
+    // Add milliseconds to start date (milliseconds/day * days)
+    endDate += MILLISECONDS_IN_A_DAY * numDaysToAdd;
+    // Create a new Date object to return
+    return new Date(endDate);
 }
