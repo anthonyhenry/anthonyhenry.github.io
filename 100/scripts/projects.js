23d14a05fa91388545f52037cc4294442ccd29c2
anthonyhenry <anthonyh1497@gmail.com>
2025-04-21 20:34:44 -0700

    Setup script for project classes

--- 100/scripts/projects.js
+++ 100/scripts/projects.js
@@ -35,3 +35,33 @@ const HOME_PAGE = new Projects(
 
 const ALL_PROJECTS = Projects.getAllInstances();
 console.log(ALL_PROJECTS)
+console.log(ALL_PROJECTS[0].name)
+
+for(let project of ALL_PROJECTS)
+{
+    for(let prop in project)
+    {
+        console.log(project[prop]);
+    }
+}
+
+// Add projects to site
+
+
+/*
+<details class="responsive-text">
+    <summary>
+    <span class="arrow"></span>
+    <span class="project-title">100 Days of Code Home Page</span>
+    <span class="summary-timeline">4/15/2025 - 4/21/2025</span>
+    </summary>
+    <img src="imgs\project-imgs\100-home-page.png" class="thumbnail">
+    <p>My first project is a home page to store links to all the projects I work on for the challenge.</p>
+    <p class="details-timeline">
+    <strong>Days</strong>: 4/15/2025 - 4/21/2025
+    </p>
+    <p>
+    <strong>Tech</strong>: HTML, CSS, JavaScript
+    </p>
+</details>
+*/
