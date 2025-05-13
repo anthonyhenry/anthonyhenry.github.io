ea3585a7320b3f93b8a77d5459e16f95bac79896
anthonyhenry <anthonyh1497@gmail.com>
2025-05-12 23:13:28 -0700

    Form validation modal

--- pomotodo/scripts/tasks.js
+++ pomotodo/scripts/tasks.js
@@ -6,4 +6,41 @@ class Tasks {
     }
 }
 
-console.log("Hello from tasks.js!")
+console.log("Hello from tasks.js!");
+
+document.querySelector("#newTaskForm").onsubmit = function()
+{
+    // Validate data
+    const DESCRIPTION = document.querySelector("#newTaskDescription").value.trim();
+    const DUE_DATE = document.querySelector("#newTaskDueDate").value;
+
+    if(DESCRIPTION.length == 0 || DUE_DATE.length == 0)
+    {
+        const INVALID_TASK_MODAL_ELEMENT = document.querySelector("#invalidTaskModal");
+        const INVALID_TASK_MODAL = new bootstrap.Modal(INVALID_TASK_MODAL_ELEMENT);;
+        const MISSING_TASK_ELEMENTS_SPAN = document.querySelector("#missingTaskElements");
+        MISSING_TASK_ELEMENTS_SPAN.innerText = "";
+
+        if(DESCRIPTION.length == 0 && DUE_DATE.length == 0)
+        {
+            MISSING_TASK_ELEMENTS_SPAN.innerText = "description and due date"
+        }
+        else if(DESCRIPTION.length == 0)
+        {
+            MISSING_TASK_ELEMENTS_SPAN.innerText += "description";
+        }
+        else if(DUE_DATE.length == 0)
+        {
+            MISSING_TASK_ELEMENTS_SPAN.innerText += "due date";
+        }
+
+        INVALID_TASK_MODAL.show();
+    }
+    
+
+    console.log(DESCRIPTION);
+    console.log(DUE_DATE);
+
+    // Block default behavior
+    return false;
+}
