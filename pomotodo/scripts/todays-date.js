17cce56845388e3ccafdcbfaf960cf2cc8361cf8
anthonyhenry <anthonyh1497@gmail.com>
2025-05-16 03:02:13 -0700

    change main.js to todays-date.js

---
+++ pomotodo/scripts/todays-date.js
@@ -0,0 +1,37 @@
+// Get today's date
+let todaysDate = new Date();
+todaysDate.setHours(0, 0, 0, 0); // Set to start of day
+
+updateDateDisplay();
+
+const MILLISECONDS_IN_A_DAY = 1000 * 1 * 60 * 60 * 24;
+
+function updateDateDisplay()
+{
+    document.querySelector("#date").innerText = todaysDate.toLocaleDateString();
+}
+
+function getTodaysDate()
+{
+    // Update todaysDate if it has passed midnight
+    const RIGHT_NOW = new Date();
+
+    if((RIGHT_NOW - todaysDate) > MILLISECONDS_IN_A_DAY)
+    {
+        todaysDate = RIGHT_NOW.setHours(0, 0, 0, 0);
+        updateDateDisplay();
+    }
+
+    return todaysDate;
+}
+
+getDaysFromNow(new Date(2025, 5-1, 14), todaysDate);
+getDaysFromNow(new Date(2025, 5-1, 15), todaysDate);
+getDaysFromNow(new Date(2025, 5-1, 17), todaysDate);
+getDaysFromNow(new Date(2025, 5-1, 18), todaysDate);
+
+
+function getDaysFromNow(date)
+{
+    console.log((date - getTodaysDate()) / MILLISECONDS_IN_A_DAY);
+}
