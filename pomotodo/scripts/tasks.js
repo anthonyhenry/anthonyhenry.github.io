d6908d8b5ae3ce597c742baf6294823b97861526
anthonyhenry <anthonyh1497@gmail.com>
2025-05-16 01:31:46 -0700

    Move validation to function

--- pomotodo/scripts/tasks.js
+++ pomotodo/scripts/tasks.js
@@ -1,4 +1,4 @@
-class Tasks {
+class Task {
     constructor(description, dueDate)
     {
         this.description = description;
@@ -6,35 +6,20 @@ class Tasks {
     }
 }
 
-console.log("Hello from tasks.js!");
-
 document.querySelector("#newTaskForm").onsubmit = function()
 {
     // Validate data
     const DESCRIPTION = document.querySelector("#newTaskDescription").value.trim();
     const DUE_DATE = document.querySelector("#newTaskDueDate").value;
 
-    if(DESCRIPTION.length == 0 || DUE_DATE.length == 0)
+    if(validateTask(DESCRIPTION, DUE_DATE))
     {
-        const INVALID_TASK_MODAL_ELEMENT = document.querySelector("#invalidTaskModal");
-        const INVALID_TASK_MODAL = new bootstrap.Modal(INVALID_TASK_MODAL_ELEMENT);;
-        const MISSING_TASK_ELEMENTS_SPAN = document.querySelector("#missingTaskElements");
-        MISSING_TASK_ELEMENTS_SPAN.innerText = "";
+        const NEW_TASK = new Task(DESCRIPTION, DUE_DATE);
+        console.log(NEW_TASK);
 
-        if(DESCRIPTION.length == 0 && DUE_DATE.length == 0)
-        {
-            MISSING_TASK_ELEMENTS_SPAN.innerText = "description and due date"
-        }
-        else if(DESCRIPTION.length == 0)
-        {
-            MISSING_TASK_ELEMENTS_SPAN.innerText += "description";
-        }
-        else if(DUE_DATE.length == 0)
-        {
-            MISSING_TASK_ELEMENTS_SPAN.innerText += "due date";
-        }
 
-        INVALID_TASK_MODAL.show();
+        console.log("Tasks validated");
+        return false;
     }
     
 
@@ -43,4 +28,35 @@ document.querySelector("#newTaskForm").onsubmit = function()
 
     // Block default behavior
     return false;
+}
+
+function validateTask(desc, dueDate)
+{
+    if(desc.length == 0 || dueDate.length == 0)
+    {
+        const INVALID_TASK_MODAL_ELEMENT = document.querySelector("#invalidTaskModal");
+        const INVALID_TASK_MODAL = new bootstrap.Modal(INVALID_TASK_MODAL_ELEMENT);;
+        const MISSING_TASK_ELEMENTS_SPAN = document.querySelector("#missingTaskElements");
+        MISSING_TASK_ELEMENTS_SPAN.innerText = "";
+
+        if(desc.length == 0 && dueDate.length == 0)
+        {
+            MISSING_TASK_ELEMENTS_SPAN.innerText = "description and due date"
+        }
+        else if(desc.length == 0)
+        {
+            MISSING_TASK_ELEMENTS_SPAN.innerText += "description";
+        }
+        else if(dueDate.length == 0)
+        {
+            MISSING_TASK_ELEMENTS_SPAN.innerText += "due date";
+        }
+
+        INVALID_TASK_MODAL.show();
+        return false;
+    }
+    else
+    {
+        return true;
+    }
 }
