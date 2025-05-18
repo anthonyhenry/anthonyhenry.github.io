b2760a07147fe9a9844766452bd62cf44c51d07e
anthonyhenry <anthonyh1497@gmail.com>
2025-05-17 17:35:59 -0700

    Create task objects

--- pomotodo/scripts/tasks.js
+++ pomotodo/scripts/tasks.js
@@ -2,23 +2,59 @@ let savedTasks = [];
 
 document.querySelector("#newTaskForm").onsubmit = function()
 {
-    getTodaysDate();
-
     // Validate data
     const DESCRIPTION = document.querySelector("#newTaskDescription").value.trim();
-    const DUE_DATE = document.querySelector("#newTaskDueDate").value;
-
-    if(validateTask(DESCRIPTION, DUE_DATE))
-    {
-        console.log("Task valid!!!")
-    }
+    let dueDate = document.querySelector("#newTaskDueDate").value;
     
+    if(validateTask(DESCRIPTION, dueDate))
+    {
+        dueDate = convertDateInputToDateObject(dueDate)
 
-    console.log(DESCRIPTION);
-    console.log(DUE_DATE);
+        const NEW_TASK = {
+            description: DESCRIPTION,
+            deadline: dueDate,
+            priority: calculatePriority(dueDate)
+        }
+
+        console.log(NEW_TASK);
+    }
 
     // Block default behavior
     return false;
+}
+
+// Function that converts a date form input into a date object
+function convertDateInputToDateObject(input)
+{
+    let date = input.split("-"); // Date inputs always come in ISO format: "YYYY-MM-DD"
+    date = new Date(date[0], date[1] - 1, date[2]); // new Date(year, monthIndex, day)
+    return date;
+}
+
+function calculatePriority(dueDate)
+{
+    const DAYS_UNTIL_DUE_DATE = ( dueDate - getTodaysDate() ) / MILLISECONDS_IN_A_DAY;
+
+    if(DAYS_UNTIL_DUE_DATE < 0)
+    {
+        return "Overdue";
+    }
+    else if(DAYS_UNTIL_DUE_DATE == 0)
+    {
+        return "Due Today";
+    }
+    else if(DAYS_UNTIL_DUE_DATE <= 3)
+    {
+        return "High";   
+    }
+    else if(DAYS_UNTIL_DUE_DATE < 7)
+    {
+        return "Medium";
+    }
+    else
+    {
+        return "Low";
+    }
 }
 
 function validateTask(desc, dueDate)
