ac55ed0e3886dcc007b11670c78dc11216949d4b
anthonyhenry <anthonyh1497@gmail.com>
2025-06-07 00:37:58 -0700

    Stickers are cloned on hover

--- stickerbook/scripts/stickers.js
+++ stickerbook/scripts/stickers.js
@@ -1,31 +1,32 @@
 // Get all sticker templates
-const STICKERS = document.querySelectorAll(".sticker");
+const STICKERS = document.querySelectorAll(".template-sticker");
 
 for(const sticker of STICKERS)
 {
-    sticker.onmouseover = function(){
+    sticker.onmouseenter = function(){
         console.log("Hovering!!!");
 
-        // Create a clone of the sticker
-        const CLONED_STICKER = sticker.cloneNode();
+        const CLONED_STICKER = document.createElement("img");
+        CLONED_STICKER.src = sticker.src;
+
 
         // Get the sticker's position relative to the viewport
-        const STICKER_RECT = sticker.getBoundingClientRect();
+        const TEMPLATE_STICKER_RECT = sticker.getBoundingClientRect();
 
         // Get canvas's position
         const CANVAS_DIV = document.querySelector("#canvas");
         const CANVAS_RECT = CANVAS_DIV.getBoundingClientRect();
 
         // Calculate sticker position relative to canvas
-        const STICKER_LEFT = STICKER_RECT.left - CANVAS_RECT.left;
-        const STICKER_TOP = STICKER_RECT.top - CANVAS_RECT.top;
+        const STICKER_LEFT = TEMPLATE_STICKER_RECT.left - CANVAS_RECT.left;
+        const STICKER_TOP = TEMPLATE_STICKER_RECT.top - CANVAS_RECT.top;
 
         // Style cloned sticker
         CLONED_STICKER.style.position = "absolute";
-        CLONED_STICKER.width = sticker.width + "px";
+        CLONED_STICKER.style.width = sticker.width + "px";
         CLONED_STICKER.style.top = STICKER_TOP + "px";
         CLONED_STICKER.style.left = STICKER_LEFT + "px";
         
-        CANVAS_DIV.appendChild(CLONED_STICKER);   
+        CANVAS_DIV.appendChild(CLONED_STICKER);
     }
 }
