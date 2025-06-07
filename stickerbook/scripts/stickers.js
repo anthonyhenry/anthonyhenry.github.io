527466ede8e69de8684b1ff9365b67f2f72866b8
anthonyhenry <anthonyh1497@gmail.com>
2025-06-07 00:00:37 -0700

    second attempt at cloning a sticker

--- stickerbook/scripts/stickers.js
+++ stickerbook/scripts/stickers.js
@@ -1,34 +1,31 @@
+// Get all sticker templates
 const STICKERS = document.querySelectorAll(".sticker");
 
 for(const sticker of STICKERS)
 {
     sticker.onmouseover = function(){
         console.log("Hovering!!!");
+
+        // Create a clone of the sticker
         const CLONED_STICKER = sticker.cloneNode();
-        // console.log(sticker.parentElement);
 
-        console.log(sticker.style)
-        console.log(sticker.style.position)
-        console.log(sticker.x)
-        console.log(sticker.y)
-        // console.log(sticker.top)
-        // console.log(sticker.left)
-        console.log(sticker.style.x)
-        console.log(sticker.style.y)
+        // Get the sticker's position relative to the viewport
+        const STICKER_RECT = sticker.getBoundingClientRect();
 
+        // Get canvas's position
+        const CANVAS_DIV = document.querySelector("#canvas");
+        const CANVAS_RECT = CANVAS_DIV.getBoundingClientRect();
+
+        // Calculate sticker position relative to canvas
+        const STICKER_LEFT = STICKER_RECT.left - CANVAS_RECT.left;
+        const STICKER_TOP = STICKER_RECT.top - CANVAS_RECT.top;
+
+        // Style cloned sticker
         CLONED_STICKER.style.position = "absolute";
-        console.log(CLONED_STICKER.style.position)
-        CLONED_STICKER.width = sticker.width;
-
-        sticker.parentElement.appendChild(CLONED_STICKER);
-        CLONED_STICKER.style.top = sticker.y + "px";
-        // CLONED_STICKER.style.left = 0;
+        CLONED_STICKER.width = sticker.width + "px";
+        CLONED_STICKER.style.top = STICKER_TOP + "px";
+        CLONED_STICKER.style.left = STICKER_LEFT + "px";
         
-
-        console.log(sticker.style.top);
-        console.log(sticker.style.left);
-
-        
+        CANVAS_DIV.appendChild(CLONED_STICKER);   
     }
-
 }
