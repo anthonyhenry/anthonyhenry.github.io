b8f4b0946cb7bae6d4ce3875431e06f9f3b84956
anthonyhenry <anthonyh1497@gmail.com>
2025-06-10 22:59:56 -0700

    First attempt at adding stickers

--- stickerbook/scripts/stickers.js
+++ stickerbook/scripts/stickers.js
@@ -3,30 +3,45 @@ const STICKERS = document.querySelectorAll(".template-sticker");
 
 for(const sticker of STICKERS)
 {
-    sticker.onmouseenter = function(){
-        console.log("Hovering!!!");
-
+    sticker.onmousedown = function(event){
+        // Create a duplicate of the sticker
         const CLONED_STICKER = document.createElement("img");
         CLONED_STICKER.src = sticker.src;
-
-
-        // Get the sticker's position relative to the viewport
-        const TEMPLATE_STICKER_RECT = sticker.getBoundingClientRect();
 
         // Get canvas's position
         const CANVAS_DIV = document.querySelector("#canvas");
         const CANVAS_RECT = CANVAS_DIV.getBoundingClientRect();
 
-        // Calculate sticker position relative to canvas
-        const STICKER_LEFT = TEMPLATE_STICKER_RECT.left - CANVAS_RECT.left;
-        const STICKER_TOP = TEMPLATE_STICKER_RECT.top - CANVAS_RECT.top;
-
         // Style cloned sticker
         CLONED_STICKER.style.position = "absolute";
         CLONED_STICKER.style.width = sticker.width + "px";
-        CLONED_STICKER.style.top = STICKER_TOP + "px";
-        CLONED_STICKER.style.left = STICKER_LEFT + "px";
-        
+
+        // Place the cloned sticker under the mouse cursor
+        CLONED_STICKER.style.left = (event.clientX - CANVAS_RECT.left - (sticker.width / 2)) + "px";
+        CLONED_STICKER.style.top = (event.clientY - CANVAS_RECT.top - (sticker.height / 2)) + "px";
+
+        // Add the sticker to the canvas
         CANVAS_DIV.appendChild(CLONED_STICKER);
+
+        function moveAt(clientX, clientY)
+        {
+            CLONED_STICKER.style.left = (clientX - CANVAS_RECT.left - (sticker.width / 2)) + "px";
+            CLONED_STICKER.style.top = (clientY - CANVAS_RECT.top - (sticker.height / 2)) + "px";
+        }
+
+        function onMouseMove(e)
+        {
+            moveAt(e.clientX, e.clientY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        CLONED_STICKER.onmouseup = function (){
+            document.removeEventListener("mousemove", onMouseMove);
+            CLONED_STICKER.onmouseup = null;
+        };
+
+        CLONED_STICKER.ondragstart = () => false; // Prevent ghost image
+
     }
 }
