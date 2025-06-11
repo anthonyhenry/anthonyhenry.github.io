e5fedc9f92cecc547c72856ff304a99e5c315d9e
anthonyhenry <anthonyh1497@gmail.com>
2025-06-11 01:35:27 -0700

    Better attempt at placing stickers

--- stickerbook/scripts/stickers.js
+++ stickerbook/scripts/stickers.js
@@ -1,47 +1,50 @@
 // Get all sticker templates
-const STICKERS = document.querySelectorAll(".template-sticker");
+const TEMPLATE_STICKERS = document.querySelectorAll(".template-sticker");
+// Get a reference to the sticker page div
+const STICKER_PAGE_DIV = document.querySelector("#stickerPage");
 
-for(const sticker of STICKERS)
+////////////////////////////////////////////////////////////////////////////////
+////////////////////// Handlers for Creating New Stickers //////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+for(const sticker of TEMPLATE_STICKERS)
 {
+    // Sticker template clicked
     sticker.onmousedown = function(event){
+        // Prevent default behavior (ghost image)
+        event.preventDefault();
+
         // Create a duplicate of the sticker
         const CLONED_STICKER = document.createElement("img");
         CLONED_STICKER.src = sticker.src;
 
-        // Get canvas's position
-        const CANVAS_DIV = document.querySelector("#canvas");
-        const CANVAS_RECT = CANVAS_DIV.getBoundingClientRect();
+        // Add the sticker to the sticker page div
+        STICKER_PAGE_DIV.appendChild(CLONED_STICKER);
 
         // Style cloned sticker
         CLONED_STICKER.style.position = "absolute";
         CLONED_STICKER.style.width = sticker.width + "px";
 
         // Place the cloned sticker under the mouse cursor
-        CLONED_STICKER.style.left = (event.clientX - CANVAS_RECT.left - (sticker.width / 2)) + "px";
-        CLONED_STICKER.style.top = (event.clientY - CANVAS_RECT.top - (sticker.height / 2)) + "px";
+        moveSticker(CLONED_STICKER, event.clientX, event.clientY)
 
-        // Add the sticker to the canvas
-        CANVAS_DIV.appendChild(CLONED_STICKER);
-
-        function moveAt(clientX, clientY)
-        {
-            CLONED_STICKER.style.left = (clientX - CANVAS_RECT.left - (sticker.width / 2)) + "px";
-            CLONED_STICKER.style.top = (clientY - CANVAS_RECT.top - (sticker.height / 2)) + "px";
+        // Move the sticker with the mouse
+        function onMouseMove(e){
+            moveSticker(CLONED_STICKER, e.clientX, e.clientY)
         }
+        document.addEventListener("mousemove", onMouseMove)
 
-        function onMouseMove(e)
-        {
-            moveAt(e.clientX, e.clientY);
-        }
-
-        document.addEventListener('mousemove', onMouseMove);
-
-        CLONED_STICKER.onmouseup = function (){
-            document.removeEventListener("mousemove", onMouseMove);
-            CLONED_STICKER.onmouseup = null;
-        };
-
-        CLONED_STICKER.ondragstart = () => false; // Prevent ghost image
-
+        // Place the sticker when the mouse is released
+        function onMouseUp() {
+			document.removeEventListener("mousemove", onMouseMove);
+			document.removeEventListener("mouseup", onMouseUp);
+		}
+		document.addEventListener("mouseup", onMouseUp);
     }
+}
+
+function moveSticker(sticker, mousePosX, mousePosY)
+{
+    sticker.style.left = mousePosX - STICKER_PAGE_DIV.offsetLeft - (sticker.width / 2) + "px";
+    sticker.style.top = mousePosY - STICKER_PAGE_DIV.offsetTop - (sticker.height / 2) + "px";
 }
