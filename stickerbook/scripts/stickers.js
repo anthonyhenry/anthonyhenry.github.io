a74bf6192aa8e09418296088a2515a273d684b50
anthonyhenry <anthonyh1497@gmail.com>
2025-06-13 00:20:44 -0700

    test function for moving places stickers

--- stickerbook/scripts/stickers.js
+++ stickerbook/scripts/stickers.js
@@ -58,6 +58,7 @@ for(const sticker of TEMPLATE_STICKERS)
                 SCENE_DIV.appendChild(CLONED_STICKER);
                 // Give the sticker the placed sticker class
                 CLONED_STICKER.classList.add("placed-sticker");
+                bindPlacedStickers();
             }
             else
             {
@@ -74,4 +75,84 @@ function moveSticker(sticker, mousePosX, mousePosY)
     // offsetLeft/Top returns the distance in pixels from the specified edge of an element to the specified edge of its nearest positioned ancestor
     sticker.style.left = mousePosX - STICKER_PAGE_DIV.offsetLeft - (sticker.width / 2) + "px"; 
     sticker.style.top = mousePosY - STICKER_PAGE_DIV.offsetTop - (sticker.height / 2) + "px";
+}
+
+
+function bindPlacedStickers()
+{
+    const PLACED_STICKERS = document.querySelectorAll(".placed-sticker");
+
+    for(sticker of PLACED_STICKERS)
+    {
+        sticker.onmousedown = function(event){
+            // Prevent default behavior (ghost image)
+            event.preventDefault();
+
+            // Add the sticker to the sticker page div
+            // STICKER_PAGE_DIV.appendChild(CLONED_STICKER);
+
+            // Place the cloned sticker under the mouse cursor
+            // moveSticker(this, event.pageX, event.pageY);
+
+            test(sticker);
+
+
+        }
+    }
+}
+
+
+function test(sticker)
+{
+    // console.log(e);
+
+    function onMouseMove(e)
+    {
+        moveSticker(sticker, e.pageX, e.pageY);
+    }
+    document.addEventListener("mousemove", onMouseMove);
+
+    // Place the sticker when the mouse is released
+    function onMouseUp() {
+
+        // Disable listeners
+        document.removeEventListener("mousemove", onMouseMove);
+        document.removeEventListener("mouseup", onMouseUp);
+
+        // Get top, right, left, bottom coordinates of the scene div
+        const SCENE_DIV = document.querySelector("#scene");
+        const SCENE_RECT = SCENE_DIV.getBoundingClientRect();
+
+        // Get top, right, left, bottom coordinates of the sticker img
+        const STICKER_RECT = sticker.getBoundingClientRect();
+        
+        // Only add the sticker if it is visible within the scene
+        if(STICKER_RECT.right >= SCENE_RECT.left 
+            && STICKER_RECT.left <= SCENE_RECT.right 
+            && STICKER_RECT.bottom >= SCENE_RECT.top 
+            && STICKER_RECT.top <= SCENE_RECT.bottom)
+        {
+            // Change the sticker's parent to the scene div
+            // SCENE_DIV.appendChild(CLONED_STICKER);
+            // Give the sticker the placed sticker class
+            // CLONED_STICKER.classList.add("placed-sticker");
+            // bindPlacedStickers();
+        }
+        else
+        {
+            // Delete stickers that aren't in the scene
+            sticker.parentElement.removeChild(sticker);
+        }
+    }
+    document.addEventListener("mouseup", onMouseUp);
+}
+
+function placeSticker(mouseMoveListenerFunc)
+{
+    function onMouseUp()
+    {
+        document.removeEventListener("mousemove", mouseMoveListenerFunc);
+        document.removeEventListener("mouseup", onMouseUp);
+    }
+    document.addEventListener("mouseup", onMouseUp);
 }
