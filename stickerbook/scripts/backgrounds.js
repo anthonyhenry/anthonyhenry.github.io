627b3d06473383d835e05ecb0ccec8d7c2e0a6e7
anthonyhenry <anthonyh1497@gmail.com>
2025-06-25 10:33:02 -0700

    Delete placed stickers when new background selected

--- stickerbook/scripts/backgrounds.js
+++ stickerbook/scripts/backgrounds.js
@@ -5,7 +5,24 @@ for(let bg of document.querySelector("#backgroundGallery").children)
     bg.onclick = function(){
         if(SCENE_BG.src != this.src)
         {
-            SCENE_BG.src = this.src;
+            const PLACED_STICKERS = document.querySelectorAll(".placed-sticker");
+
+            if(PLACED_STICKERS.length > 0)
+            {
+                if(confirm("There are stickers placed on the current scene. Changing the background will remove all placed stickers."))
+                {
+                    for (let sticker of PLACED_STICKERS)
+                    {
+                        removeElement(sticker);
+                    }
+
+                    SCENE_BG.src = this.src;
+                }
+            }
+            else
+            {
+                SCENE_BG.src = this.src;
+            }
         }
     }
 }
