256bd0579ff16594bb905e4c0eb8e7a49d0b24cf
anthonyhenry <anthonyh1497@gmail.com>
2025-07-11 02:53:19 -0700

    Keep scroll when resizing screen

--- stickerbook/flexbox-test/scripts/stickers.js
+++ stickerbook/flexbox-test/scripts/stickers.js
@@ -2,11 +2,12 @@
 ////////////////////////// Sticker Gallery Scrolling //////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-// Bug when you scroll all the way as far right or down as you can go then resize the screen. Both arrows are invisible
-// Ask chatGPT if I should add mouse events
-
 const LEFT_UP_ARROW = document.querySelector("#left-up-arrow");
 const RIGHT_DOWN_ARROW = document.querySelector("#right-down-arrow");
+const STICKERS_CONTAINER = document.querySelector("#stickers");
+
+let scrollAxis = getScrollAxis()
+let scrollAmount = getScrollAmount();
 
 let scrollAnimationFrameRequest = null;
 
@@ -16,6 +17,7 @@ for(let arrow of [LEFT_UP_ARROW, RIGHT_DOWN_ARROW])
     {
         cancelAnimationFrame(scrollAnimationFrameRequest);
         scrollAnimationFrameRequest = null;
+        scrollAmount = getScrollAmount();
     }
 
     function scrollStickers(event)
@@ -24,13 +26,12 @@ for(let arrow of [LEFT_UP_ARROW, RIGHT_DOWN_ARROW])
 
         const SCROLL_DIRECTION = arrow.dataset.direction == "left-up" ? -1 : 1;
         const SCROLL_SPEED = 3;
-        const STICKERS_CONTAINER = document.querySelector("#stickers");
-        const SCROLL_AXIS = window.getComputedStyle(STICKERS_CONTAINER).display == "block" ? "vertical" : "horizontal";
+        // scrollAxis = getScrollAxis();
         const SCROLL_OPTIONS = {
-            top: SCROLL_AXIS == "vertical" ? SCROLL_DIRECTION * SCROLL_SPEED : 0,
-            left: SCROLL_AXIS == "horizontal" ? SCROLL_DIRECTION * SCROLL_SPEED : 0,
+            top: scrollAxis == "vertical" ? SCROLL_DIRECTION * SCROLL_SPEED : 0,
+            left: scrollAxis == "horizontal" ? SCROLL_DIRECTION * SCROLL_SPEED : 0,
             behavior: "auto"
-        }
+        } // This is an object that will be passed through STICKERS_CONTAINER.scrollBy
 
         function scroll()
         {
@@ -38,7 +39,7 @@ for(let arrow of [LEFT_UP_ARROW, RIGHT_DOWN_ARROW])
             STICKERS_CONTAINER.scrollBy(SCROLL_OPTIONS);
 
             // Only show arrows if there is room to scroll in that direction
-            if(SCROLL_AXIS == "vertical")
+            if(scrollAxis == "vertical")
             {
                 if(STICKERS_CONTAINER.scrollTop > 0 && window.getComputedStyle(LEFT_UP_ARROW).display == "none")
                 {
@@ -103,69 +104,57 @@ for(let arrow of [LEFT_UP_ARROW, RIGHT_DOWN_ARROW])
     arrow.addEventListener("touchcancel", stopScrolling);
 }
 
-const stickersContainer = document.getElementById("stickers");
-const arrowUpOrLeft = document.querySelector(".arrow.left-up");
-const arrowDownOrRight = document.querySelector(".arrow.right-down");
-
-
-function isVerticalLayout() 
-{
-    return window.innerWidth >= 768;
-}
-
-function updateArrows()
-{
-    if (isVerticalLayout()) 
+window.addEventListener("resize", function(){
+    // Check if screen was resized such that the stickers div changed scroll direction
+    if(scrollAxis != getScrollAxis())
     {
-        arrowUpOrLeft.style.display = stickersContainer.scrollTop > 0 ? "block" : "none";
-        arrowDownOrRight.style.display = stickersContainer.scrollTop + stickersContainer.clientHeight < stickersContainer.scrollHeight ? "block" : "none";
-    } 
-    else 
-    {
-        arrowUpOrLeft.style.display = stickersContainer.scrollLeft > 0 ? "block" : "none";
-        arrowDownOrRight.style.display = stickersContainer.scrollLeft + stickersContainer.clientWidth < stickersContainer.scrollWidth ? "block" : "none";
+        let percentScrolled = 0;
+
+        if(scrollAxis == "vertical")
+        {
+            percentScrolled = scrollAmount.vertical / scrollAmount.height;
+            STICKERS_CONTAINER.scrollLeft = percentScrolled * STICKERS_CONTAINER.scrollWidth;            
+        }
+        else // Horizontal scroll axis
+        {
+            percentScrolled = scrollAmount.horizontal / scrollAmount.width;
+            STICKERS_CONTAINER.scrollTop = percentScrolled * STICKERS_CONTAINER.scrollHeight;
+
+            if(STICKERS_CONTAINER.clientHeight + STICKERS_CONTAINER.scrollTop != STICKERS_CONTAINER.scrollHeight)
+            {
+                RIGHT_DOWN_ARROW.style.display = "block";
+            }
+        }        
+        scrollAmount = getScrollAmount();
+        scrollAxis = getScrollAxis();
     }
-}
+    else
+    {
+        
+    }
 
-function startScrolling(direction)
+    if(STICKERS_CONTAINER.clientWidth + STICKERS_CONTAINER.scrollLeft != STICKERS_CONTAINER.scrollWidth)
+    {
+        RIGHT_DOWN_ARROW.style.display = "block";
+    }
+    if(scrollAxis == "horizontal" && STICKERS_CONTAINER.clientWidth + STICKERS_CONTAINER.scrollLeft == STICKERS_CONTAINER.scrollWidth)
+    {
+        RIGHT_DOWN_ARROW.style.display = "none";
+    }
+})
+
+function getScrollAmount()
 {
-    // event.preventDefault();
-    // const amount = 5; // smaller = smoother scrolling
-    // stopScrolling(); // clear any existing interval first
-    // scrollInterval = setInterval(() => {
-    //     if (isVerticalLayout())
-    //     {
-    //         stickersContainer.scrollBy({ top: direction * amount, behavior: "auto" });
-    //     } 
-    //     else
-    //     {
-    //         stickersContainer.scrollBy({ left: direction * amount, behavior: "auto" });
-    //     }
-    // }, 16); // roughly 60 frames per second
+    const SCROLL_AMOUNT = {
+        vertical: STICKERS_CONTAINER.scrollTop,
+        horizontal: STICKERS_CONTAINER.scrollLeft,
+        height: STICKERS_CONTAINER.scrollHeight,
+        width: STICKERS_CONTAINER.scrollWidth
+    };
+    return SCROLL_AMOUNT;
 }
 
-// function stopScrolling()
-// {
-//     if (scrollInterval !== null)
-//     {
-//         clearInterval(scrollInterval);
-//         scrollInterval = null;
-//     }
-// }
-
-// Attach press-and-hold listeners
-// arrowUpOrLeft.addEventListener("mousedown", () => startScrolling(-1));
-// arrowDownOrRight.addEventListener("mousedown", () => startScrolling(1));
-// document.addEventListener("mouseup", stopScrolling);
-// arrowUpOrLeft.addEventListener("mouseleave", stopScrolling);
-// arrowDownOrRight.addEventListener("mouseleave", stopScrolling);
-
-// // Mobile touch support
-// arrowUpOrLeft.addEventListener("touchstart", () => startScrolling(-1));
-// arrowDownOrRight.addEventListener("touchstart", () => startScrolling(1));
-// document.addEventListener("touchend", stopScrolling);
-// document.addEventListener("touchcancel", stopScrolling);
-
-// stickersContainer.addEventListener("scroll", updateArrows);
-// window.addEventListener("resize", updateArrows);
-// window.addEventListener("DOMContentLoaded", updateArrows);
+function getScrollAxis()
+{
+    return window.getComputedStyle(STICKERS_CONTAINER).display == "block" ? "vertical" : "horizontal";
+}
